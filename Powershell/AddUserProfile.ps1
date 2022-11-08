3b2f85c2f31fa302e21e6692a7ff06a5cbf6af30
Thomas Gruber <tgruber@globalblue.com>
2022-11-08 08:57:09 +0100

    upidate

--- Powershell/AddUserProfile.ps1
+++ Powershell/AddUserProfile.ps1
@@ -1,1 +1,1 @@
-Write-Output ". $(Get-Item .\PSProfile.ps1 | ForEach-Object { $_.FullName })" > "C:\Users\tgr\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+Write-Output ". $(Get-Item .\PSProfile.ps1 | ForEach-Object { $_.FullName })" > "$profile"
