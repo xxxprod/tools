9b22a8ad67e18d31bb3aaa184927bb66c0edfb00
xxxprod <tgr@lookat.at>
2022-11-08 18:33:01 +0100

    update

--- Powershell/PSProfile.ps1
+++ Powershell/PSProfile.ps1
@@ -8,7 +8,7 @@ function gclone { git clone $args }
 function gcheckout { git checkout $args }
 function gadd { git add $args }
 function greset { git reset $args }
-function gcommit { git commit -m $args -join ' ' }
+function gcommit { git commit -m $($args -join ' ') }
 function gpull { git pull }
 function gpush { git push }
 
